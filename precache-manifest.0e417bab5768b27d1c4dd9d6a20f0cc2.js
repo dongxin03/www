0ed876f086da7cceb14dd7b792b67e9bc37511f6
董新 <dongxin@huohua.cn>
2019-06-25 17:05:36 +0800

    add

---
+++ precache-manifest.0e417bab5768b27d1c4dd9d6a20f0cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763fbde10d2025685f23723a0a763f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e28ba119408034acd3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e7fa8feb4c6ab67b06a2",
+    "url": "/static/js/2.ecb39a0c.chunk.js"
+  },
+  {
+    "revision": "d0e28ba119408034acd3",
+    "url": "/static/js/main.4106101f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
